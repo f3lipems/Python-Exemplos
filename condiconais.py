55e1924a0a318f6379fc70dc0894de4b9b0499aa
Felipe Martins da Silva <felipe@visionar.io>
2018-06-08 19:05:50 -0300

    Adicionado for.py

--- condiconais.py
+++ condiconais.py
@@ -1,15 +1,41 @@
+## Condicionais simples
+
+idioma = 'portugues'
+
+if idioma == 'portugues':
+    print('Idioma {} dfinido como padrão.'.format(idioma)) 
+
+
+## Condicionais compostas
+
+idioma = 'espanhol'
+
+if idioma == 'ingles':
+    print('Idioma {} dfinido como padrão.'.format(idioma)) 
+else:
+    print('Idioma não disponível')
+
+
+## Condicionais aninhadas
+
 idade = 19
 
 if idade < 18:
     print('Idade insuficiente para o acesso.')
-elif (idade >= 18 and idade < 21):
+elif idade >= 18 and idade < 21:
     print('Acesso liberado, com restrição.')
 else:
     print('Acesso liberado.')
 
-pessoa = 'crianca'
+
+pessoa = 'idosa'
 
 if pessoa == 'mulher' or pessoa == 'crianca':
     print('Isento')
+elif pessoa in 'idosa idoso':
+    if pessoa == 'idosa':
+        print('Entrada com desconto e tem direito a brinde.')
+    else:
+        print('Entrada com desconto.')
 else:
-    print('Cobrar entrada')
+    print('Valor integral')
---
+++ for.py
@@ -0,0 +1,17 @@
+## Repetições com for
+
+for i in range(10,15):
+    print(i)
+
+for i in range(5):
+    print('Número {}'.format(i))
+
+for i in range(10,20,2):
+    print(i)
+
+for i in range(10,0,-2):
+    print(i)
+
+tabuada = 7
+for j in range(1,11):
+    print('{} x {} = {}'.format(j, tabuada, j*tabuada))
